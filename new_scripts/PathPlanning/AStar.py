dc5fe83782fd0c778605dc6660f3bcc4a593383e
LBBassani <lorenabassani12@gmail.com>
2019-10-17 21:54:28 -0300

    Definição de função heurística para A*

--- new_scripts/PathPlanning/AStar.py
+++ new_scripts/PathPlanning/AStar.py
@@ -17,3 +17,9 @@ class AStar(IPathPlanning):
     @staticmethod
     def PathPlan(graph, start, goal):
         pass
+
+    @staticmethod
+    def __heuristic(a, b):
+        (x1, y1) = a
+        (x2, y2) = b
+        return abs(x1 - x2) + abs(y1 - y2)
