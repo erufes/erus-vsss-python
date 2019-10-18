e573dc14f481cbd7b7fbc02e9aa17f0c6de7a873
LBBassani <lorenabassani12@gmail.com>
2019-10-17 21:36:26 -0300

    Nova função de Reconstrução de Caminho

--- new_scripts/PathPlanning/IPathPlanning.py
+++ new_scripts/PathPlanning/IPathPlanning.py
@@ -11,3 +11,14 @@ class IPathPlanning(object):
     @staticmethod
     def PathPlan(graph, start, goal):
         raise NotImplementedError
+
+    @staticmethod
+    def reconstructPath(cameFrom, start, goal):
+        current = goal
+        path = []
+        while current != start:
+            path.append(current)
+            current = cameFrom[current]
+        path.append(start) # optional
+        path.reverse() # optional
+        return path
