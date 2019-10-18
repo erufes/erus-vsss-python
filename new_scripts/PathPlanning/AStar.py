d2d2cc5120125665b23de522b9841b2e4bb36a3a
LBBassani <lorenabassani12@gmail.com>
2019-10-17 22:36:21 -0300

    Rascunho AStar

--- new_scripts/PathPlanning/AStar.py
+++ new_scripts/PathPlanning/AStar.py
@@ -3,9 +3,13 @@
     Descrição do módulo :   Algoritmo A* para Planejameto de Trajetória
     Versão :                1.0
     Pré-requisitos :        IPathPlanning
+                            WeightedGridGraph
+                            PriorityQueue
     Membros :               Lorena Bassani
 """
 from .IPathPlanning import IPathPlanning
+from .Graph import WeightedGridGraph
+from queue import PriorityQueue
 
 """ A* is a modification of Dijkstra’s Algorithm that is optimized for 
     a single destination. Dijkstra’s Algorithm can find paths to all 
@@ -16,7 +20,28 @@ class AStar(IPathPlanning):
 
     @staticmethod
     def PathPlan(graph, start, goal):
-        pass
+        # FIXME : Está percorrendo todo o mapa, não apenas na direção do objetivo
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[start] = None
+        cost_so_far[start] = 0
+
+        while not frontier.empty():
+            current = frontier.get()            
+            if current == goal:
+                break
+            neighbors = graph.neighbors(current)
+            for prox, occ in neighbors:
+                new_cost = cost_so_far[current] + graph.cost(current, prox)
+                if occ is None and (prox not in cost_so_far or new_cost < cost_so_far[prox]):
+                    cost_so_far[prox] = new_cost
+                    priority = new_cost + AStar.__heuristic(graph.transform2Cart(goal), graph.transform2Cart(prox))
+                    frontier.put(prox, priority)
+                    came_from[prox] = current
+
+        return came_from, cost_so_far
 
     @staticmethod
     def __heuristic(a, b):
