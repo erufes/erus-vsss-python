90e6ea5c791c11fd5e7acb22b7367aca424e11dd
LBBassani <lorenabassani12@gmail.com>
2019-10-17 22:16:07 -0300

    Implementação do Algoritmo de Dijkstra

--- new_scripts/PathPlanning/DijkstraAlgorithm.py
+++ new_scripts/PathPlanning/DijkstraAlgorithm.py
@@ -3,10 +3,13 @@
     Descrição do módulo :   Algoritmo de Dijkstra para busca de caminho em grafos
     Versão :                1.0
     Pré-requisitos :        IPathPlanning
+                            WeightedGridGraph
+                            PriorityQueue
     Membros :               Lorena Bassani
 """
-
+from .Graph import WeightedGridGraph
 from .IPathPlanning import IPathPlanning
+from queue import PriorityQueue
 
 """ Instead of exploring all possible paths equally, it favors lower cost paths. 
     We can assign lower costs to encourage moving on roads, higher costs to avoid 
@@ -17,4 +20,24 @@ class DijkstraAlgorithm(IPathPlanning):
 
     @staticmethod
     def PathPlan(graph, start, goal):
-        pass
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[start] = None
+        cost_so_far[start] = 0
+        
+        while not frontier.empty():
+            current = frontier.get()            
+            if current == goal:
+                break
+            neighbors = graph.neighbors(current)
+            for prox, occ in neighbors:
+                new_cost = cost_so_far[current] + graph.cost(current, prox)
+                if occ is None and (prox not in cost_so_far or new_cost < cost_so_far[prox]):
+                    cost_so_far[prox] = new_cost
+                    priority = new_cost
+                    frontier.put(prox, priority)
+                    came_from[prox] = current
+        
+        return came_from, cost_so_far
