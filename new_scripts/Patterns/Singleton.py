eb8450a332c09ba59bf79df46d97acb6bb2d68ed
LBBassani <lorenabassani12@gmail.com>
2019-10-19 12:11:20 -0300

    SingletonException e Documentação

--- new_scripts/Patterns/Singleton.py
+++ new_scripts/Patterns/Singleton.py
@@ -1,5 +1,24 @@
+""" Nome do módulo :        Singleton
+    Ano de criação :        2019/10
+    Descrição do módulo :   Módulo que implementa padrão Singleton
+    Versão :                1.0
+    Pré-requisitos :        Nenhum
+    Membros :               Lorena B. Bassani
+"""
+
+class SingletonException(Exception):
+    pass
+
 class Singleton(object):
    __instance = None
+
+   """ Nome da função :      getInstance
+        Intenção da função : Retornar a instância da classe
+        Pré-requisitos :     Nenhum
+        Efeitos colaterais : Nova instância pode ser criada se ainda não houver uma
+        Parâmetros :         Nenhum
+        Retorno :            Singleton : Instância do Singleton
+    """
    @staticmethod 
    def getInstance():
       """ Static access method. """
@@ -9,6 +28,6 @@ class Singleton(object):
    def __init__(self):
       """ Virtually private constructor. """
       if Singleton.__instance != None:
-         raise Exception("This class is a singleton!")
+         raise SingletonException
       else:
          Singleton.__instance = self
