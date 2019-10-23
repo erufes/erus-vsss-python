2b9d7dfdad48f709e08214c695dc8411e6f373b2
LBBassani <lorenabassani12@gmail.com>
2019-10-23 13:50:21 -0300

    Rascunho de Jogo

--- new_scripts/Mundo.py
+++ new_scripts/Mundo.py
@@ -13,16 +13,23 @@ from .Jogador import Jogador
 from .ComportamentosJogadores.Comportamentos import COMPORTAMENTOS
 from .Ball import Ball
 from .Campo import Campo
+from .Controle.ControleTrajeto.IControleTrajeto import IcontroleTrajeto
+from .Controle.ControleTrajeto.ControleSiegwart import ControleSiegwart
+from .PathPlanning.IPathPlanning import IPathPlanning
+from .PathPlanning.AStar import AStar
+import math as m
 
 class Mundo(Singleton):
 
     def __init__(self, *args, **keyargs):
         pass
 
-    def inicializa(self):
+    def inicializa(self, controladorTrajeto = ControleSiegwart, pathPlanning = AStar):
         self.__jogadores = {"Team" : list(), "Enemies" : list()}
         self.ball = Ball()
         self.campo = Campo(celulasX = 15, celulasY = 13)
+        self.pathPlanning = AStar
+        self.controladorTrajeto = controladorTrajeto
     
     """ Nome da função :     inimigos (getter)
         Intenção da função : Retorna os Inimigos
@@ -86,3 +93,26 @@ class Mundo(Singleton):
         if p:
             return p
         return None
+    
+    def control(self):
+        self.__defineFunction()
+        controle = list()
+        for p in self.__jogadores["Team"]:
+            # Primeiro passo: Definir Objetivo
+            goal = p.definirObjetivo(self)
+            start = p.posicao
+            # Segundo passo: Planejar Caminho
+            path = self.pathPlanning.PathPlan(self.campo, self.campo.transform2Grid(start), self.campo.transform2Grid(goal))
+            path = self.pathPlanning.reconstructPath(path, self.campo.transform2Grid(start), self.campo.transform2Grid(goal))
+            # Terceiro passo: Seguir Caminho
+            gx, gy = self.campo.transform2Cart(path.pop(0))
+            sx, sy = start
+            gt = m.acos((gx*sx + gy*sy)/(m.sqrt(gx**2 + gy**2)*m.sqrt(sx**2 + sy**2)))
+            goal = gx, gy, gt
+            start = sx, sy, p.theta
+            vel = self.controladorTrajeto.controle(start, goal, 100)
+            controle.append(vel)
+        return controle
+
+    def __defineFunction(self):
+        pass
