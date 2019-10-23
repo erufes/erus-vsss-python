eff48e1b3c31c1a0e357f60d0b4e2bf72d213b30
LBBassani <lorenabassani12@gmail.com>
2019-10-23 16:15:50 -0300

    Documentação ComportamentoLissajous

--- new_scripts/ComportamentosJogadores/ComportamentoLissajous.py
+++ new_scripts/ComportamentosJogadores/ComportamentoLissajous.py
@@ -1,6 +1,11 @@
-""" Na matemática, a curva de Lissajous (figura de Lissajous ou curva de Bowditch) 
-    é o gráfico produzido por um sistema de equações paramétricas que descreve um complexo movimento harmônico.
-    x = A*sen(at + sig), y = B*sen(bt)
+""" Nome do módulo :        ComportamentoLissajous
+    Ano de criação :        2019/10
+    Descrição do módulo :   Modela um comportamento que descreve uma curva de Lissajous
+    Versão :                1.0
+    Pré-requisitos :        IComportamento
+                            Geometria
+                            math
+    Membros :               Lorena B Bassani
 """
 from .IComportamento import IComportamento
 from ..Geometria import Ponto
@@ -21,6 +26,10 @@ class ComportamentoLissajous(IComportamento):
         self.__t = 0
     
     def definirObjetivo(self, jogador, Mundo):
+        """ Na matemática, a curva de Lissajous (figura de Lissajous ou curva de Bowditch) 
+            é o gráfico produzido por um sistema de equações paramétricas que descreve um complexo movimento harmônico.
+            x = A*sen(at + sig), y = B*sen(bt)
+        """
         x = self.A*m.sin(self.a*self.__t + self.sigma)
         y = self.B*m.sin(self.b*self.__t)
         self.__t += 1
